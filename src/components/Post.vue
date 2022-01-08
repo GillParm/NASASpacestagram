d07eaf9815600de021c952b35799db68baeb23cd
Parman Gill <g.parm@outlook.com>
2022-01-08 10:16:46 -0500

    spacestagram 1st commit

---
+++ src/components/Post.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="post-container">
+    <div class="image-container">
+      <img class="image" loading="lazy" :src="img" :alt="title">
+    </div>
+    <div class="text-container">
+      <h3>{{ title }} - {{ date }}</h3>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Post',
+  props: {
+    img: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.post-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: white;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.2);
+}
+.image-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.image {
+  max-width: 100%;
+  max-height: 100%;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+.text-container {
+  padding: 0 15px 0 15px;
+}
+h3 {
+  font-size: 1rem;
+}
+p {
+  font-size: 0.8rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,212 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div  class="app-container">
+    <div v-show="images.length < 1" class="loader"></div>
+    <div v-show="images.length > 0" class="feed-container">
+      <h1>Spacestagram</h1>
+      <h2>Brought to you by NASA's Astronomy Photo of the Day (APOD) API</h2>
+      <Post v-for="(img,i) in images"
+            :key="i"
+            :img="img.url"
+            :title="img.title"
+            :description="img.explanation"
+            :date="img.date"
+      />
+      <div class="loader2"></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import axios from 'axios'
+import Post from '@/components/Post.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
+    Post
+  },
+  data () {
+    return {
+      images: [] as Array<never>,
+      datePicker: 6
+    }
+  },
+  computed: {
+    currentDate () {
+      const date = new Date()
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1
+      const day = date.getDate()
+      return `${year}-${month}-${day}`
+    },
+    endDate () {
+      const date = new Date()
+      date.setDate(date.getDate() - this.datePicker)
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1
+      const day = date.getDate()
+      return `${year}-${month}-${day}`
+    }
+  },
+  methods: {
+    getNextImages () {
+      window.onscroll = async () => {
+        const bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight
+        if (bottomOfWindow) {
+          try {
+            const prevEndDate = this.endDate
+            this.datePicker += 6
+            const response = await axios.get(`${process.env.VUE_APP_NASA_API_URL}?api_key=${process.env.VUE_APP_NASA_API_KEY}&start_date=${this.endDate}&end_date=${prevEndDate}`)
+            response.data.pop()
+            this.images = this.images.concat(response.data.reverse())
+          } catch (error) {
+            console.log(error)
+          }
+        }
+      }
+    }
+  },
+  async beforeMount () {
+    try {
+      const response = await axios.get(`${process.env.VUE_APP_NASA_API_URL}?api_key=${process.env.VUE_APP_NASA_API_KEY}&start_date=${this.endDate}&end_date=${this.currentDate}`)
+      this.images = response.data.reverse()
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  mounted () {
+    this.getNextImages()
   }
 })
 </script>
+<style scoped lang="scss">
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f5f5f5;
+  max-width:450px;
+  margin: 0 auto;
+}
+.feed-container {
+  width: 90%;
+}
+h2 {
+  font-size: 0.8rem;
+  font-weight: normal;
+  margin-bottom: 1rem;
+  color: #777777;
+  @media (max-width: 768px) {
+    font-size: 0.7rem;
+  }
+}
+
+h1 {
+  font-size: 1.5rem;
+}
+//loader css from https://codepen.io/ivillamil/pen/dokmG
+.loader  {
+  animation: rotate 1s infinite;
+  height: 50px;
+  width: 50px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -50px;
+}
+
+.loader:before,
+.loader:after {
+  border-radius: 50%;
+  content: '';
+  display: block;
+  height: 20px;
+  width: 20px;
+}
+.loader:before {
+  animation: ball1 1s infinite;
+  background-color: #cb2025;
+  box-shadow: 30px 0 0 #f8b334;
+  margin-bottom: 10px;
+}
+.loader:after {
+  animation: ball2 1s infinite;
+  background-color: #00a096;
+  box-shadow: 30px 0 0 #97bf0d;
+}
+
+@keyframes rotate {
+  0% {
+    -webkit-transform: rotate(0deg) scale(0.8);
+    -moz-transform: rotate(0deg) scale(0.8);
+  }
+  50% {
+    -webkit-transform: rotate(360deg) scale(1.2);
+    -moz-transform: rotate(360deg) scale(1.2);
+  }
+  100% {
+    -webkit-transform: rotate(720deg) scale(0.8);
+    -moz-transform: rotate(720deg) scale(0.8);
+  }
+}
+
+@keyframes ball1 {
+  0% {
+    box-shadow: 30px 0 0 #f8b334;
+  }
+  50% {
+    box-shadow: 0 0 0 #f8b334;
+    margin-bottom: 0;
+    -webkit-transform: translate(15px,15px);
+    -moz-transform: translate(15px, 15px);
+  }
+  100% {
+    box-shadow: 30px 0 0 #f8b334;
+    margin-bottom: 10px;
+  }
+}
+
+@keyframes ball2 {
+  0% {
+    box-shadow: 30px 0 0 #97bf0d;
+  }
+  50% {
+    box-shadow: 0 0 0 #97bf0d;
+    margin-top: -20px;
+    -webkit-transform: translate(15px,15px);
+    -moz-transform: translate(15px, 15px);
+  }
+  100% {
+    box-shadow: 30px 0 0 #97bf0d;
+    margin-top: 0;
+  }
+}
+.loader2  {
+  animation: rotate 1s infinite;
+  height: 50px;
+  width: 50px;
+  margin: 0 auto;
+}
+
+.loader2:before,
+.loader2:after {
+  border-radius: 50%;
+  content: '';
+  display: block;
+  height: 20px;
+  width: 20px;
+}
+.loader2:before {
+  animation: ball1 1s infinite;
+  background-color: #cb2025;
+  box-shadow: 30px 0 0 #f8b334;
+  margin-bottom: 10px;
+}
+.loader2:after {
+  animation: ball2 1s infinite;
+  background-color: #00a096;
+  box-shadow: 30px 0 0 #97bf0d;
+}
+</style>
